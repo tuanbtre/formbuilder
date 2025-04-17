c3f2c9e6b6615d25e44ccf2681afcdefb44f7c61
Tuan Nguyen Minh <tuannm@tdt.local>
2025-04-17 17:51:52 +0700

    update

--- src/Routers/web.php
+++ src/Routers/web.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Tuanbtre\FormBuilder\Http\Controllers\FormController;
 
-Route::prefix('form-builder')->name('form-builder.')->group(function () {
-    Route::get('/forms', [FormController::class, 'index'])->name('forms.index');
-    Route::get('/forms/create', [FormController::class, 'create'])->name('forms.create');
-    Route::post('/forms', [FormController::class, 'store'])->name('forms.store');
-    Route::post('/forms/{form}/submit', [FormController::class, 'submit'])->name('forms.submit');
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::any('/forms', [FormController::class, 'index'])->name('form.index')->middleware('checkright');
+    //Route::get('/forms/create', [FormController::class, 'create'])->name('forms.create');
+    //Route::post('/forms', [FormController::class, 'store'])->name('forms.store');
+    //Route::post('/forms/{form}/submit', [FormController::class, 'submit'])->name('forms.submit');
 });
 
 Route::get('/', [FormController::class, 'showPublic'])->name('form-builder.home');
