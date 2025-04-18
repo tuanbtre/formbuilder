5c2cba89597dc857823feb5a02d0aff96c3f2654
Tuan Nguyen Minh <tuannm@tdt.local>
2025-04-18 11:44:55 +0700

    update

--- src/FormBuilderServiceProvider.php
+++ src/FormBuilderServiceProvider.php
@@ -9,22 +9,22 @@ class FormBuilderServiceProvider extends ServiceProvider
     public function boot()
     {
         // Đăng ký routes
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/Routes/web.php');
 
         // Đăng ký views
-        $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'form-builder');
+        $this->loadViewsFrom(__DIR__ . '/Resources/views', 'form-builder');
 
         // Đăng ký migrations
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/Database/Migrations');
 
         // Publish views để tùy chỉnh
         $this->publishes([
-            __DIR__ . '/../Resources/views' => resource_path('views/Admin/form-builder'),
+            __DIR__ . '/Resources/views' => resource_path('views/Admin/form-builder'),
         ], 'form-builder-views');
 
         // Publish migrations để tùy chỉnh
         $this->publishes([
-            __DIR__ . '/../Database/Migrations' => database_path('migrations'),
+            __DIR__ . '/Database/Migrations' => database_path('migrations'),
         ], 'form-builder-migrations');
     }
 
